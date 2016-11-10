5bcbeecb297f670e7c35ea6fb17097f687b4e936
rwinterhalder <richard.winterhalder@bwinparty.com>
2016-11-10 09:50:15 +0100

    -splited SameKey in SameKeySameValue and SameKeyOtherValue -removed the Seperator,

--- src/at/rwinterhalder/cc/Main.java
+++ src/at/rwinterhalder/cc/Main.java
@@ -4,22 +4,23 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-        String seperator = "<SEPERATOR>";
-        String fileName1 = "C:\\Users\\rwinterhalder\\Desktop\\Lucky7\\disme config\\fvt\\happ\\happ_fvt_final.properties";
-        String fileName2 = "C:\\Users\\rwinterhalder\\Desktop\\Lucky7\\disme config\\fvt\\happ\\happ_fvtv2.properties";
-        String outputFilename = "C:\\Users\\rwinterhalder\\Desktop\\Lucky7\\disme config\\fvt\\happ\\happ_fvt_final_cc_happ_fvtv2.properties";
+        String fileName1 = "C:\\Users\\rwinterhalder\\Desktop\\Lucky7\\disme config\\CC_Test\\t1.properties";
+        String fileName2 = "C:\\Users\\rwinterhalder\\Desktop\\Lucky7\\disme config\\CC_Test\\t2.properties";
+        String outputFilename = "C:\\Users\\rwinterhalder\\Desktop\\Lucky7\\disme config\\CC_Test\\o.properties";
 
         Map<String,String> properties1 = new TreeMap<>();
         readFile(fileName1, properties1);
 
         Map<String,String> properties2 = new TreeMap<>();
         readFile(fileName2, properties2);
-        Map<String,String> sameProperties = new TreeMap<>();
+
+        Map<String,Value> sameProperties = new TreeMap<>();
 
 
         Set<String> uniqueKeys = new HashSet<>();
@@ -28,23 +29,31 @@ public class Main {
 
         for(final Map.Entry<String, String> entry : properties1.entrySet()) {
             if(!properties2.containsKey(entry.getKey())){
-                uniqueKeys1.add(entry.getKey());
+                uniqueKeys1.add(entry.getKey().trim());
             }else{
-                sameProperties.put(entry.getKey(), entry.getValue());
+                sameProperties.put(entry.getKey(), new Value(entry.getValue().trim()));
             }
         }
 
         for(final Map.Entry<String, String> entry : properties2.entrySet()) {
             if(!properties1.containsKey(entry.getKey())){
-                uniqueKeys2.add(entry.getKey());
+                uniqueKeys2.add(entry.getKey().trim());
             }else{
-                if(!sameProperties.get(entry.getKey()).equals(entry.getValue())){
-                    sameProperties.put(entry.getKey(),sameProperties.get(entry.getKey()) + seperator + entry.getValue());
-                }
+                sameProperties.get(entry.getKey()).setVal2(entry.getValue().trim());
             }
         }
         uniqueKeys.addAll(uniqueKeys1);
         uniqueKeys.addAll(uniqueKeys2);
+
+        //SAME-KEY-SAME-VALUE
+        Map<String,Value> samePropertiesSameValue = sameProperties.entrySet().stream()
+                .filter(val -> val.getValue().isSame() == true)
+                .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
+
+        //SAME-KEY-OTHER-VALUE
+        Map<String,Value> samePropertiesOtherValue = sameProperties.entrySet().stream()
+                .filter(val -> val.getValue().isSame() == false)
+                .collect(Collectors.toMap(p -> p.getKey(), p -> p.getValue()));
 
         // Write File
         try {
@@ -60,22 +69,25 @@ public class Main {
             writeMap(properties2, uniqueKeys2, out);
             out.write("\n");
 
-            out.write(String.format("SAME-KEYS: Count: %s\n", sameProperties.size()));
-            for(Map.Entry<String, String> entry : sameProperties.entrySet()){
+            out.write(String.format("SAME-KEYS-SAME-VALUES: Count: %s\n", samePropertiesSameValue.size()));
+
+            for(Map.Entry<String, Value> entry : samePropertiesSameValue.entrySet()){
                 out.write(entry.getKey());
-                String[] val = entry.getValue().split(seperator,2);
-                if(val.length == 2){
-                    out.write("=");
-                    out.write(String.format("%s\n",val[0]));
-                    for(int i = 0; i < entry.getKey().length(); i++){
-                        out.write(" ");
-                    }
-                    out.write("=");
-                    out.write(String.format("%s\n",val[1]));
-                }else{
-                    out.write("=");
-                    out.write(String.format("%s\n",val[0]));
+                out.write("=");
+                out.write(String.format("%s\n",entry.getValue().getVal1()));
+            }
+            out.write("\n");
+
+            out.write(String.format("SAME-KEYS-OTHER-VALUES: Count: %s\n", samePropertiesOtherValue.size()));
+            for(Map.Entry<String, Value> entry : samePropertiesSameValue.entrySet()){
+                out.write(entry.getKey());
+                out.write("=");
+                out.write(String.format("%s\n",entry.getValue().getVal1()));
+                for(int i = 0; i < entry.getKey().length(); i++){
+                    out.write(" ");
                 }
+                out.write("=");
+                out.write(String.format("%s\n",entry.getValue().getVal2()));
             }
 
             out.close();
@@ -98,13 +110,61 @@ public class Main {
         try (Stream<String> lines = Files.lines(Paths.get(fileName))) {
 
            lines.forEach((x) -> {
-                String[] line = x.split("=", 2);
-               if(line.length == 2)
-                properties.put(line[0].trim(), line[1].trim());
+               String[] line = x.split("=", 2);
+               if(readRules(line)) {
+                   properties.put(line[0].trim(), line[1].trim());
+               }
            });
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static boolean readRules(String[] line){
+        if(line.length != 2){
+            return false;
+        }
+        if(line[0].startsWith("#")){
+            return false;
+        }
+        return true;
+    }
+
+    private static class Value{
+        private String val1;
+        private String val2;
+        private boolean same;
+
+        public Value(String val1) {
+            this.val1 = val1;
+            this.val2 = null;
+            compare();
+        }
+
+        public String getVal1() {
+            return val1;
+        }
+
+        public String getVal2() {
+            return val2;
+        }
+
+        public void setVal2(String val2) {
+            this.val2 = val2;
+            compare();
+        }
+
+        private void compare(){
+            if(val1.equals(val2)){
+                same = true;
+            }else {
+                same = false;
+            }
+        }
+
+        public boolean isSame() {
+            return same;
         }
     }
 }
